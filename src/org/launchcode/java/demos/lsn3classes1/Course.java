bcad38447e0d056b4e5f5aa8cea820d57aae06af
Gus <gus.stobbs@gmail.com>
2022-01-14 16:32:13 -0600

    completed chapter 4 exercises

---
+++ src/org/launchcode/java/demos/lsn3classes1/Course.java
@@ -0,0 +1,9 @@
+package org.launchcode.java.demos.lsn3classes1;
+
+import java.util.ArrayList;
+
+public class Course {
+    private String subject;
+    private Teacher teacher;
+    private ArrayList<Student> students;
+}
--- src/org/launchcode/java/demos/lsn3classes1/SchoolPractice.java
+++ src/org/launchcode/java/demos/lsn3classes1/SchoolPractice.java
@@ -3,5 +3,18 @@ package org.launchcode.java.demos.lsn3classes1;
 public class SchoolPractice {
     public static void main(String[] args) {
         // Instantiate your Student class for part 2 here!
+        Student name;
+        name = new Student();
+        name.setName("Gus");
+        name.setStudentId(1);
+        name.setNumberOfCredits(1);
+        name.setGpa(4.0);
+
+        System.out.println("Name: " + name.getName());
+        System.out.println("Student ID: " + name.getStudentId());
+        System.out.println("Number of credits: " + name.getNumberOfCredits());
+        System.out.println("GPA: " + name.getGpa());
+
+
     }
 }
